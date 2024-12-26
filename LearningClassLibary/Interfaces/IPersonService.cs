e48d0b1448491c4830bec763a1376dd902db6b8d
Guy  Cizner <46866327+ciznerguy@users.noreply.github.com>
2024-12-26 21:12:13 +0200

    Add project files.

---
+++ LearningClassLibary/Interfaces/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LearningClassLibary.Models; // Update to the correct namespace of the Person class.
+
+namespace LearningClassLibary.Interfaces
+{
+    public interface IPersonService
+    {
+        Task<List<Person>> GetAllPersonsAsync();
+        Task<Person?> GetPersonByIdAsync(int id);
+        Task AddPersonAsync(Person person);
+        Task UpdatePersonAsync(Person person);
+        Task DeletePersonAsync(int id);
+    }
+}
